0fd05c7986bdca818d959d81725291ef4c1c565c
KerekesDavid <drakediskeve@gmail.com>
2024-06-27 14:11:03 +0200

    Update readme, uncomment tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -9,17 +9,21 @@ class testModelBuild(unittest.TestCase):
     def setUp(self):
         # TODO should we just glob these for convinience?
         self.datasets = {
-            # 'mados': 'configs/datasets_config/mados.yaml',
+            'mados': 'configs/datasets_config/mados.yaml',
             'sen1floods11': 'configs/datasets_config/sen1floods11.yaml',
-            # 'burn_scars': 'configs/datasets_config/burn_scars.yaml',
-            # 'croptypemapping': 'configs/datasets_config/croptypemapping.yaml',
+            'burn_scars': 'configs/datasets_config/burn_scars.yaml',
+            'croptypemapping': 'configs/datasets_config/croptypemapping.yaml',
         }
 
-    def test_download(self):
+    def test_dataset(self):
         for dataset in self.datasets.keys():
             with self.subTest(dataset=dataset):
                 print(f"Testing dataset {dataset}")
                 cfg = {'dataset_config': self.datasets[dataset]}
                 dataset_cfg = load_specific_config(cfg, 'dataset_config')
 
-                train_ds, val_ds, test_ds = make_dataset(dataset_cfg)
+                dataset_splits = make_dataset(dataset_cfg)
+
+                for ds in dataset_splits:
+                    input = next(iter(ds))
+                    self.assertTrue(input) # TODO some sanity checks here based on the config file
