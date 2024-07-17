f1f82a47d4f06b46ba156a4ecf990a82b0339bae
Yuru Jia <yuruj@kth.se>
2024-07-17 04:43:21 +0200

    zero padding image

--- engine/data_preprocessor.py
+++ engine/data_preprocessor.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 
 import numpy as np
+import pdb
 
 class DataPreprocessor():
     def __init__(self, args, encoder_cfg, dataset_cfg, logger):
@@ -45,9 +46,7 @@ class OpticalPreprocessor():
         self.used_bands_mask = torch.tensor([b in self.input_bands for b in self.dataset_bands], dtype=torch.bool)
         self.avail_bands_mask = torch.tensor([b in self.dataset_bands for b in self.input_bands], dtype=torch.bool)
         # self.used_bands_indices = torch.tensor([self.dataset_bands.index(b) if b in self.input_bands else -1 for b in self.dataset_bands], dtype=torch.long)
-        self.avail_bands_indices = torch.tensor(
-            [self.dataset_bands.index(b) if b in self.dataset_bands else -1 for b in self.input_bands],
-            dtype=torch.long)
+        self.avail_bands_indices = [self.dataset_bands.index(b) if b in self.dataset_bands else None for b in self.input_bands]
         self.need_padded = self.avail_bands_mask.sum() < len(self.input_bands)
 
         self.logger = logger
@@ -60,19 +59,37 @@ class OpticalPreprocessor():
 
 
     def __call__(self, optical_image):
+        # padded_image = torch.cat([torch.zeros_like(optical_image[:, 0: 1]), optical_image], dim=1)
+        # optical_image = padded_image[:, self.avail_bands_indices + 1]
 
-        padded_image = torch.cat([torch.zeros_like(optical_image[:, 0: 1]), optical_image], dim=1)
-        optical_image = padded_image[:, self.avail_bands_indices + 1]
+        # optical_image = F.interpolate(optical_image, (self.input_size, self.input_size), mode='bilinear', align_corners=False)
 
-        optical_image = F.interpolate(optical_image, (self.input_size, self.input_size), mode='bilinear', align_corners=False)
+        # if self.temporal_input and len(optical_image.shape) == 4:
+        #     optical_image = optical_image.unsqueeze(2)
 
-        if self.temporal_input and len(optical_image.shape) == 4:
+        # return optical_image
+
+
+
+        dtype = optical_image.dtype
+        device = optical_image.device
+
+        if self.temporal_input and optical_image.dim() == 4:
             optical_image = optical_image.unsqueeze(2)
 
-        return optical_image
+        if optical_image.dim() == 4:
+            B, _, H, W = optical_image.shape
+            zero_tensor = torch.zeros(B, 1, H, W, dtype=dtype, device=device)
+        elif optical_image.dim() == 5:  # For tensors of shape (B, C, T, H, W)
+            B, _, T, H, W = optical_image.shape
+            zero_tensor = torch.zeros(B, 1, T, H, W, dtype=dtype, device=device)
+        else:
+            raise ValueError("Unsupported tensor shape")
 
+        out_tensors = [optical_image[:, [i], ...] if i is not None else zero_tensor for i in self.avail_bands_indices]   
+        out_tensors = torch.concat(out_tensors, dim=1).to(device)
 
+        out_tensors = F.interpolate(out_tensors, (self.input_size, self.input_size), mode='bilinear', align_corners=False)
 
-
-
+        return out_tensors
 
