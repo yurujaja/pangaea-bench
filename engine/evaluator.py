a1f8ff07f3c19a07614b312dbc3483cc477ff992
Yuru Jia <yuruj@kth.se>
2024-07-24 17:43:31 +0200

    add f1 metric

--- engine/evaluator.py
+++ engine/evaluator.py
@@ -1,10 +1,8 @@
-import torch
-import torch.nn.functional as F
-
 import time
 from tqdm import tqdm
-import numpy as np
 import logging
+import torch
+
 
 class Evaluator():
     def __init__(self, args, val_loader, exp_dir, device):
@@ -67,16 +65,63 @@ class SegEvaluator(Evaluator):
     def __call__(self, model, model_name='model'):
         return self.evaluate(model, model_name)
 
-
     def compute_metrics(self, confusion_matrix):
-        iou = torch.diag(confusion_matrix) / (confusion_matrix.sum(dim=1) + confusion_matrix.sum(dim=0) - torch.diag(confusion_matrix)) * 100
+        # Calculate IoU for each class
+        intersection = torch.diag(confusion_matrix)
+        union = confusion_matrix.sum(dim=1) + confusion_matrix.sum(dim=0) - intersection
+        iou = (intersection / (union + 1e-6)) * 100
+
+        # Calculate precision and recall for each class
+        precision = intersection / (confusion_matrix.sum(dim=0) + 1e-6) * 100
+        recall = intersection / (confusion_matrix.sum(dim=1) + 1e-6) * 100
+
+        # Calculate F1-score for each class
+        f1 = 2 * (precision * recall) / (precision + recall + 1e-6)
+
+        # Calculate mean IoU, mean F1-score, and mean Accuracy
+        miou = iou.mean().item()
+        mf1 = f1.mean().item()
+        macc = (intersection.sum() / (confusion_matrix.sum() + 1e-6)).item() * 100
+
+        # Convert metrics to CPU and to Python scalars
         iou = iou.cpu()
-        metrics = {'IoU': [iou[i].item() for i in range(self.num_classes)], 'mIoU': iou.mean().item()}
+        f1 = f1.cpu()
+        precision = precision.cpu()
+        recall = recall.cpu()
+
+        # Prepare the metrics dictionary
+        metrics = {
+            'IoU': [iou[i].item() for i in range(self.num_classes)],
+            'mIoU': miou,
+            'F1': [f1[i].item() for i in range(self.num_classes)],
+            'mF1': mf1,
+            'mAcc': macc,
+            'Precision': [precision[i].item() for i in range(self.num_classes)],
+            'Recall': [recall[i].item() for i in range(self.num_classes)]
+        }
 
         return metrics
 
     def log_metrics(self, metrics):
-        header = "------- IoU --------\n"
-        iou = '\n'.join(c.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % num) for c, num in zip(self.classes, metrics['IoU'])) + '\n'
-        miou = "-------------------\n" + 'Mean'.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % metrics['mIoU'])
-        self.logger.info(header+iou+miou)
+        def format_metric(name, values, mean_value):
+            header = f"------- {name} --------\n"
+            metric_str = '\n'.join(c.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % num) for c, num in zip(self.classes, values)) + '\n'
+            mean_str = "-------------------\n" + 'Mean'.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % mean_value)
+            return header + metric_str + mean_str
+
+        iou_str = format_metric("IoU", metrics['IoU'], metrics['mIoU'])
+        f1_str = format_metric("F1-score", metrics['F1'], metrics['mF1'])
+        
+        precision_mean = torch.tensor(metrics['Precision']).mean().item()
+        recall_mean = torch.tensor(metrics['Recall']).mean().item()
+        
+        precision_str = format_metric("Precision", metrics['Precision'], precision_mean)
+        recall_str = format_metric("Recall", metrics['Recall'], recall_mean)
+        
+        macc_str = f"Mean Accuracy: {metrics['mAcc']:.3f} \n"
+
+        self.logger.info(iou_str)
+        self.logger.info(f1_str)
+        self.logger.info(precision_str)
+        self.logger.info(recall_str)
+        self.logger.info(macc_str)
