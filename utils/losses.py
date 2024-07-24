1988fa635037b0e624d7d1b936ea6627e04d5892
Yuru Jia <yuruj@kth.se>
2024-07-24 17:42:49 +0200

    add dice loss

--- utils/losses.py
+++ utils/losses.py
@@ -1,10 +1,45 @@
 import torch
 from utils.registry import LOSS_REGISTRY
+from torch.nn import functional as F
+
 
 @LOSS_REGISTRY.register()
 def CrossEntropy(cfg):
     return torch.nn.CrossEntropyLoss(ignore_index = cfg["ignore_index"])
 
+
 @LOSS_REGISTRY.register()
 def MSELoss(cfg):
     return torch.nn.MSELoss()
+
+
+@LOSS_REGISTRY.register()
+class DICELoss(torch.nn.Module):
+    def __init__(self, cfg):
+        super(DICELoss, self).__init__()
+        self.ignore_index = cfg["ignore_index"]
+    
+    def forward(self, logits, target):
+        # Convert logits to probabilities using softmax
+        probs = F.softmax(logits, dim=1)
+        
+        num_classes = logits.shape[1]
+        mask = (target != self.ignore_index)
+        #mask_expand = mask.unsqueeze(1).expand_as(probs)
+        target_temp = target.clone()
+        target_temp[~mask] = 0
+
+        target_one_hot = F.one_hot(target_temp, num_classes=num_classes).permute(0, 3, 1, 2).float()
+        target_one_hot = target_one_hot * mask.unsqueeze(1).float()
+
+        intersection = torch.sum(probs * target_one_hot, dim=(2, 3))
+        union = torch.sum(probs + target_one_hot, dim=(2, 3))
+
+        dice_score = (2. * intersection + 1e-6) / (union + 1e-6)
+        # dice_loss = 1 - dice_score.mean(dim=1).mean()
+        valid_dice = dice_score[mask.any(dim=(1, 2))]
+        dice_loss = 1 - valid_dice.mean()  # Dice loss is 1 minus the Dice score
+        
+        return dice_loss
+
+    
