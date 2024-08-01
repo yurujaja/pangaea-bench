f442683f8b56c077ce7f9ab64559b804754a5143
Yuru Jia <yuruj@kth.se>
2024-08-01 11:08:00 +0200

    enable dofa support s1

--- foundation_models/dofa_encoder.py
+++ foundation_models/dofa_encoder.py
@@ -193,8 +193,11 @@ class DOFA_Encoder(Base_Encoder):
 
     def forward(self, image):
         # embed patches
-        x = image['optical']
+        # x = image['optical']
+        x = [image[m] for m in self.input_bands.keys()]
+        x = torch.cat(x, dim=1)
         wavelist = torch.tensor(self.wv_list, device=x.device).float()
+        # import pdb; pdb.set_trace()
         self.waves = wavelist
 
         x, _ = self.patch_embed(x, self.waves)
@@ -216,4 +219,3 @@ class DOFA_Encoder(Base_Encoder):
                 output.append(out)
 
         return output
-
