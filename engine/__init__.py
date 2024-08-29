adc7014bb3c115ec2d0b6b9e0e000f9641510f02
gle-bellier <georges.lebellier@gmail.com>
2024-08-29 18:01:45 +0200

    Refactor collate_fn, next step is ts padding (#12)

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,13 @@
 from .trainer import SegTrainer, RegTrainer
 from .evaluator import SegEvaluator, RegEvaluator
-from .data_preprocessor import SegPreprocessor, RegPreprocessor
+from .data_preprocessor import SegPreprocessor, RegPreprocessor, get_collate_fn
 
-all = ['SegTrainer', 'SegEvaluator', 'SegPreprocessor', 'RegTrainer', 'RegEvaluator', 'RegPreprocessor']
+all = [
+    "SegTrainer",
+    "SegEvaluator",
+    "SegPreprocessor",
+    "RegTrainer",
+    "RegEvaluator",
+    "RegPreprocessor",
+]
+
