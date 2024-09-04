71d13feea3fd81b51ae62abef82e6de1cec97e1e
SebastianHafner <hafnersailing@gmail.com>
2024-09-04 10:58:08 +0200

    no changes

--- run.py
+++ run.py
@@ -18,27 +18,28 @@ from engine import SegPreprocessor, SegEvaluator, SegTrainer, RegEvaluator, RegT
 from foundation_models.utils import download_model
 
 import utils.optimizers
-import utils.schedulers 
+import utils.schedulers
 import utils.losses
 from utils.utils import fix_seed, get_generator, seed_worker, prepare_input
 from utils.logger import init_logger
 from utils.configs import load_configs
-from utils.registry import ENCODER_REGISTRY, SEGMENTOR_REGISTRY, DATASET_REGISTRY, OPTIMIZER_REGISTRY, SCHEDULER_REGISTRY, LOSS_REGISTRY, AUGMENTER_REGISTRY
+from utils.registry import ENCODER_REGISTRY, SEGMENTOR_REGISTRY, DATASET_REGISTRY, OPTIMIZER_REGISTRY, \
+    SCHEDULER_REGISTRY, LOSS_REGISTRY, AUGMENTER_REGISTRY
 
 parser = argparse.ArgumentParser(description="Train a downstream task with geospatial foundation models.")
 
 master_config_group = parser.add_mutually_exclusive_group(required=True)
 master_config_group.add_argument("--config", type=str, default="",
-                    help="Master config file path for training")
+                                 help="Master config file path for training")
 master_config_group.add_argument("--eval_dir", type=str, default="",
-                    help="Path to the working directory of a model to be evaluated")
+                                 help="Path to the working directory of a model to be evaluated")
 parser.add_argument("--dataset_config", dest="dataset_config_path",
                     help="train config file path")
 parser.add_argument("--encoder_config", dest="encoder_config_path",
                     help="train config file path")
 parser.add_argument("--segmentor_config", dest="segmentor_config_path",
                     help="train config file path")
-parser.add_argument("--augmentation_config",dest="augmentation_config_path",
+parser.add_argument("--augmentation_config", dest="augmentation_config_path",
                     help="train config file path")
 parser.add_argument("--finetune", action="store_true",
                     help="fine tune whole networks")
@@ -77,7 +78,6 @@ parser.add_argument("--eval_interval", type=int,
 parser.add_argument("--log_interval", type=int,
                     help="log interval in iterations")
 
-
 parser.add_argument('--rank', type=int,
                     help='rank of current process')
 parser.add_argument('--local_rank', type=int,
@@ -86,6 +86,7 @@ parser.add_argument('--world_size', type=int,
                     help="world size")
 parser.add_argument('--local_world_size', type=int,
                     help="local world size")
+
 
 def main():
     cfg = load_configs(parser)
@@ -106,10 +107,10 @@ def main():
     dataset_name = cfg.dataset.dataset_name
     task_name = cfg.segmentor.task_name
     segmentor_name = cfg.segmentor.segmentor_name
-        
+
     # setup a work directory and logger
     timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-    
+
     if not cfg.eval_dir:
         exp_name = f"{timestamp}-{encoder_name}-{segmentor_name}-{dataset_name}-{task_name}"
         exp_dir = pathlib.Path(cfg.work_dir) / exp_name
@@ -118,12 +119,11 @@ def main():
 
         config_log_dir = exp_dir / 'configs'
         config_log_dir.mkdir(exist_ok=True)
-        OmegaConf.save(cfg, config_log_dir/'config.yaml')
+        OmegaConf.save(cfg, config_log_dir / 'config.yaml')
     else:
         exp_dir = pathlib.Path(cfg.eval_dir)
         exp_name = exp_dir.name
         logger_path = exp_dir / 'test.log'
-    
 
     logger = init_logger(logger_path, rank=cfg.rank)
     logger.info("============ Initialized logger ============")
@@ -139,12 +139,12 @@ def main():
             name=exp_name,
             config=OmegaConf.to_container(cfg, resolve=True),
         )
-    
+
     # get datasets
     dataset = DATASET_REGISTRY.get(cfg.dataset.dataset_name)
     dataset.download(cfg.dataset, silent=False)
     train_dataset, val_dataset, test_dataset = dataset.get_splits(cfg.dataset)
-    
+
     # Apply data processing to the datasets
     for step in cfg.augmentation.train:
         train_dataset = AUGMENTER_REGISTRY.get(step)(train_dataset, cfg, cfg.augmentation.train[step])
@@ -157,11 +157,10 @@ def main():
     logger.info(f"   Training: {pprint.pformat([s for s in cfg.augmentation.train])}")
     logger.info(f"   Evaluation: {pprint.pformat([s for s in cfg.augmentation.test])}")
 
-
     # TODO: Move this somewhere in data preprocessing
     def collate_data(batch):
         return {
-            'image': {modality:torch.stack([x['image'][modality] for x in batch]) for modality in cfg.dataset.bands},
+            'image': {modality: torch.stack([x['image'][modality] for x in batch]) for modality in cfg.dataset.bands},
             'target': torch.stack([x['target'] for x in batch])
         }
 
@@ -176,29 +175,30 @@ def main():
     if missing:
         logger.warning("Missing parameters:\n" + "\n".join("%s: %s" % (k, v) for k, v in sorted(missing.items())))
     if incompatible_shape:
-        logger.warning("Incompatible parameters:\n" + "\n".join("%s: expected %s but found %s" % (k, v[0], v[1]) for k, v in sorted(incompatible_shape.items())))
+        logger.warning("Incompatible parameters:\n" + "\n".join(
+            "%s: expected %s but found %s" % (k, v[0], v[1]) for k, v in sorted(incompatible_shape.items())))
 
     # prepare the segmentor
     model = SEGMENTOR_REGISTRY.get(cfg.segmentor.segmentor_name)(cfg, cfg.segmentor, encoder).to(device)
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[cfg.local_rank], output_device=cfg.local_rank)
     logger.info("Built {} for with {} encoder.".format(model.module.model_name, encoder.model_name))
 
-    # training 
+    # training
     if not cfg.eval_dir:
 
         if 0 < cfg.limited_label < 1:
-            indices = random.sample(range(len(train_dataset)), int(len(train_dataset)*cfg.limited_label))
+            indices = random.sample(range(len(train_dataset)), int(len(train_dataset) * cfg.limited_label))
             train_dataset = Subset(train_dataset, indices)
-            perc = cfg.limited_label*100
+            perc = cfg.limited_label * 100
             logger.info(f"Created a subset of the train dataset, with {perc}% of the labels available")
         else:
             logger.info(f"The entire train dataset will be used.")
-        
+
         # get train val data loaders
         train_loader = DataLoader(
             train_dataset,
             sampler=DistributedSampler(train_dataset),
-            batch_size=cfg.batch_size,#cfg.dataset["batch"],
+            batch_size=cfg.batch_size,  # cfg.dataset["batch"],
             num_workers=cfg.num_workers,
             pin_memory=True,
             persistent_workers=True,
@@ -214,8 +214,8 @@ def main():
             num_workers=cfg.num_workers,
             pin_memory=True,
             persistent_workers=False,
-            #worker_init_fn=seed_worker,
-            #generator=g,
+            # worker_init_fn=seed_worker,
+            # generator=g,
             drop_last=False,
             collate_fn=collate_data
         )
@@ -226,7 +226,9 @@ def main():
         # TODO: Make this not drop the first training sample
         train_features = next(iter(train_loader))
         input_res = tuple(train_features["image"]["optical"].size())
-        macs, params = ptflops.get_model_complexity_info(model=model, input_res=input_res, input_constructor=prepare_input, as_strings=True, backend='pytorch', verbose=True)
+        macs, params = ptflops.get_model_complexity_info(model=model, input_res=input_res,
+                                                         input_constructor=prepare_input, as_strings=True,
+                                                         backend='pytorch', verbose=True)
         logger.info(f"Model MACs: {macs}")
         logger.info(f"Model Params: {params}")
 
@@ -240,30 +242,32 @@ def main():
 
         # build scheduler
         total_iters = cfg.epochs * len(train_loader)
-        scheduler = SCHEDULER_REGISTRY.get(cfg.segmentor.scheduler.scheduler_name)(optimizer, total_iters, cfg.segmentor.scheduler)
+        scheduler = SCHEDULER_REGISTRY.get(cfg.segmentor.scheduler.scheduler_name)(optimizer, total_iters,
+                                                                                   cfg.segmentor.scheduler)
         logger.info("Built {} scheduler.".format(str(type(scheduler))))
-
 
         # training: put all components into engines
         if task_name == "regression":
             val_evaluator = RegEvaluator(cfg, val_loader, exp_dir, device)
-            trainer = RegTrainer(cfg, model, train_loader, criterion, optimizer, scheduler, val_evaluator, exp_dir, device)
+            trainer = RegTrainer(cfg, model, train_loader, criterion, optimizer, scheduler, val_evaluator, exp_dir,
+                                 device)
         else:
             val_evaluator = SegEvaluator(cfg, val_loader, exp_dir, device)
-            trainer = SegTrainer(cfg, model, train_loader, criterion, optimizer, scheduler, val_evaluator, exp_dir, device)
-        
+            trainer = SegTrainer(cfg, model, train_loader, criterion, optimizer, scheduler, val_evaluator, exp_dir,
+                                 device)
+
         # resume training if model_checkpoint is provided
         if cfg.resume_from is not None:
             trainer.load_model(cfg.resume_from)
 
         trainer.train()
-    
+
     # Evaluation
     else:
         test_loader = DataLoader(
             test_dataset,
             sampler=DistributedSampler(test_dataset),
-            batch_size=cfg.batch_size,                       
+            batch_size=cfg.batch_size,
             num_workers=cfg.num_workers,
             pin_memory=True,
             persistent_workers=False,
@@ -283,6 +287,7 @@ def main():
 
     if cfg.use_wandb and cfg.rank == 0:
         wandb.finish()
+
 
 if __name__ == "__main__":
     main()
