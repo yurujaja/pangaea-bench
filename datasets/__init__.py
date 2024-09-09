95ea04dc56ff28e15ed268ce398dde83cd3439ae
Kerekes Dávid <drakediskeve.gmail.com>
2024-09-09 18:11:56 +0200

    Disable ai4smallfarms and fivebillionpixels until they are fixed.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,6 @@ from .sen1floods11 import Sen1Floods11
 from .xview2 import xView2
 from .biomassters import BioMassters
 from .croptypemapping import CropTypeMappingSouthSudan
-from .ai4smallfarms import AI4SmallFarms
+# from .ai4smallfarms import AI4SmallFarms
 from .spacenet7 import SN7MAPPING, SN7CD
-from .fivebillionpixels import FiveBillionPixels
+# from .fivebillionpixels import FiveBillionPixels
