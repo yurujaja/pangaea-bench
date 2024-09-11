eea42858a1caf5c3d8ec2ef4e15a2c6df667be55
Kerekes Dávid <drakediskeve@gmail.com>
2024-09-11 10:46:14 +0200

    Add some preliminary tests and a github action for them (#30)
    * Add some preliminary tests
    
    * Add test github workflow
    
    * Enable workflow on test branch
    
    * Add badge, remove defaults channel
    
    * Add env cache
    
    * Learn to spell
    
    * Cache the correct miniconda dir
    
    * See below
    
    * Update actions to latest version
    
    * Update actions to latest version again
    
    * Enable linting
    
    * Fix linting?
    
    * Fix linting?
    
    * Fix linting?

---
+++ tests/test_imports.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class testPackageImports(unittest.TestCase):
+    def test_datasets(self):
+        import datasets
+
+    def test_foundation_models(self):
+        import foundation_models
+
+    def test_segmentors(self):
+        import segmentors
+
+    def test_engine(self):
+        import engine
+
+    def test_run(self):
+        import run
