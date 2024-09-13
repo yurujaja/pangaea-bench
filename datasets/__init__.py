ce5688fb670f67bf3041a38a3d52e0f731c7acf9
gle-bellier <georges.lebellier@gmail.com>
2024-09-13 16:16:38 +0200

    Update datasets inits

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,10 +1,11 @@
-from .mados import MADOS
-from .hlsburnscars import HLSBurnScars
-from .sen1floods11 import Sen1Floods11
-from .xview2 import xView2
+from .ai4smallfarms import AI4SmallFarms
 from .biomassters import BioMassters
 from .croptypemapping import CropTypeMappingSouthSudan
-from .ai4smallfarms import AI4SmallFarms
-from .spacenet7 import SN7MAPPING, SN7CD
 from .fivebillionpixels import FiveBillionPixels
+from .hlsburnscars import HLSBurnScars
+from .mados import MADOS
+from .pastis import Pastis
+from .sen1floods11 import Sen1Floods11
+from .spacenet7 import SN7CD, SN7MAPPING
 from .utae_dynamicen import DynamicEarthNet
+from .xview2 import xView2
