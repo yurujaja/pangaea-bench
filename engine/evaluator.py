9212e80d8e8ade9698ca7e88898d74a61602a541
KerekesDavid <drakediskeve@gmail.com>
2024-09-17 13:46:47 +0200

    Re-add todos to evaluator

--- engine/evaluator.py
+++ engine/evaluator.py
@@ -5,7 +5,8 @@ import logging
 import torch
 import torch.nn.functional as F
 
-class Evaluator():
+
+class Evaluator:
     def __init__(self, args, val_loader, exp_dir, device):
 
         self.args = args
@@ -13,7 +14,7 @@ class Evaluator():
         self.logger = logging.getLogger()
         self.exp_dir = exp_dir
         self.device = device
-        #self.cls_name
+        # self.cls_name
         self.classes = self.val_loader.dataset.classes
         self.split = self.val_loader.dataset.split
         self.num_classes = len(self.classes)
@@ -21,8 +22,8 @@ class Evaluator():
 
         if args.use_wandb:
             import wandb
-            self.wandb = wandb
 
+            self.wandb = wandb
 
     def __call__(self, model):
         pass
@@ -39,13 +40,13 @@ class SegEvaluator(Evaluator):
         super().__init__(args, val_loader, exp_dir, device)
 
     @torch.no_grad()
-    def evaluate(self, model, model_name='model', model_ckpt_path=None):
+    def evaluate(self, model, model_name="model", model_ckpt_path=None):
         t = time.time()
 
         if model_ckpt_path is not None:
             model_dict = torch.load(model_ckpt_path, map_location=self.device)
-            model_name = os.path.basename(model_ckpt_path).split('.')[0]
-            if 'model' in model_dict:
+            model_name = os.path.basename(model_ckpt_path).split(".")[0]
+            if "model" in model_dict:
                 model.module.load_state_dict(model_dict["model"])
             else:
                 model.module.load_state_dict(model_dict)
@@ -54,11 +55,13 @@ class SegEvaluator(Evaluator):
 
         model.eval()
 
-        tag = f'Evaluating {model_name} on {self.split} set'
-        confusion_matrix = torch.zeros((self.num_classes, self.num_classes), device=self.device)
+        tag = f"Evaluating {model_name} on {self.split} set"
+        confusion_matrix = torch.zeros(
+            (self.num_classes, self.num_classes), device=self.device
+        )
 
         for batch_idx, data in enumerate(tqdm(self.val_loader, desc=tag)):
-            image, target = data['image'], data['target']
+            image, target = data["image"], data["target"]
             image = {k: v.to(self.device) for k, v in image.items()}
             target = target.to(self.device)
 
@@ -69,10 +72,14 @@ class SegEvaluator(Evaluator):
                 pred = torch.argmax(logits, dim=1)
             valid_mask = target != -1
             pred, target = pred[valid_mask], target[valid_mask]
-            count = torch.bincount((pred * self.num_classes + target), minlength=self.num_classes ** 2)
+            count = torch.bincount(
+                (pred * self.num_classes + target), minlength=self.num_classes**2
+            )
             confusion_matrix += count.view(self.num_classes, self.num_classes)
 
-        torch.distributed.all_reduce(confusion_matrix, op=torch.distributed.ReduceOp.SUM)
+        torch.distributed.all_reduce(
+            confusion_matrix, op=torch.distributed.ReduceOp.SUM
+        )
         metrics = self.compute_metrics(confusion_matrix)
         self.log_metrics(metrics)
 
@@ -81,7 +88,7 @@ class SegEvaluator(Evaluator):
         return metrics, used_time
 
     @torch.no_grad()
-    def __call__(self, model, model_name='model', model_ckpt_path=None):
+    def __call__(self, model, model_name="model", model_ckpt_path=None):
         return self.evaluate(model, model_name, model_ckpt_path)
 
     def compute_metrics(self, confusion_matrix):
@@ -110,13 +117,13 @@ class SegEvaluator(Evaluator):
 
         # Prepare the metrics dictionary
         metrics = {
-            'IoU': [iou[i].item() for i in range(self.num_classes)],
-            'mIoU': miou,
-            'F1': [f1[i].item() for i in range(self.num_classes)],
-            'mF1': mf1,
-            'mAcc': macc,
-            'Precision': [precision[i].item() for i in range(self.num_classes)],
-            'Recall': [recall[i].item() for i in range(self.num_classes)]
+            "IoU": [iou[i].item() for i in range(self.num_classes)],
+            "mIoU": miou,
+            "F1": [f1[i].item() for i in range(self.num_classes)],
+            "mF1": mf1,
+            "mAcc": macc,
+            "Precision": [precision[i].item() for i in range(self.num_classes)],
+            "Recall": [recall[i].item() for i in range(self.num_classes)],
         }
 
         return metrics
@@ -124,19 +131,29 @@ class SegEvaluator(Evaluator):
     def log_metrics(self, metrics):
         def format_metric(name, values, mean_value):
             header = f"------- {name} --------\n"
-            metric_str = '\n'.join(c.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % num) for c, num in zip(self.classes, values)) + '\n'
-            mean_str = "-------------------\n" + 'Mean'.ljust(self.max_name_len, ' ') + '\t{:>7}'.format('%.3f' % mean_value)
+            metric_str = (
+                "\n".join(
+                    c.ljust(self.max_name_len, " ") + "\t{:>7}".format("%.3f" % num)
+                    for c, num in zip(self.classes, values)
+                )
+                + "\n"
+            )
+            mean_str = (
+                "-------------------\n"
+                + "Mean".ljust(self.max_name_len, " ")
+                + "\t{:>7}".format("%.3f" % mean_value)
+            )
             return header + metric_str + mean_str
 
-        iou_str = format_metric("IoU", metrics['IoU'], metrics['mIoU'])
-        f1_str = format_metric("F1-score", metrics['F1'], metrics['mF1'])
-        
-        precision_mean = torch.tensor(metrics['Precision']).mean().item()
-        recall_mean = torch.tensor(metrics['Recall']).mean().item()
-        
-        precision_str = format_metric("Precision", metrics['Precision'], precision_mean)
-        recall_str = format_metric("Recall", metrics['Recall'], recall_mean)
-        
+        iou_str = format_metric("IoU", metrics["IoU"], metrics["mIoU"])
+        f1_str = format_metric("F1-score", metrics["F1"], metrics["mF1"])
+
+        precision_mean = torch.tensor(metrics["Precision"]).mean().item()
+        recall_mean = torch.tensor(metrics["Recall"]).mean().item()
+
+        precision_str = format_metric("Precision", metrics["Precision"], precision_mean)
+        recall_str = format_metric("Recall", metrics["Recall"], recall_mean)
+
         macc_str = f"Mean Accuracy: {metrics['mAcc']:.3f} \n"
 
         self.logger.info(iou_str)
@@ -146,7 +163,7 @@ class SegEvaluator(Evaluator):
         self.logger.info(macc_str)
 
         if self.args.use_wandb and self.args.rank == 0:
-           self.wandb.log(
+            self.wandb.log(
                 {
                     "val_mIoU": metrics["mIoU"],
                     "val_mF1": metrics["mF1"],
@@ -164,27 +181,30 @@ class SegEvaluator(Evaluator):
                 }
             )
 
+
 class RegEvaluator(Evaluator):
     def __init__(self, args, val_loader, exp_dir, device):
         super().__init__(args, val_loader, exp_dir, device)
 
     @torch.no_grad()
-    def evaluate(self, model, model_name='model'):
+    def evaluate(self, model, model_name="model"):
+        # TODO: Rework this to allow evaluation only runs
+        # Move common parts to parent class, and get loss function from the registry.
         t = time.time()
 
         model.eval()
 
-        tag = f'Evaluating {model_name} on {self.split} set'
+        tag = f"Evaluating {model_name} on {self.split} set"
 
         for batch_idx, data in enumerate(tqdm(self.val_loader, desc=tag)):
-            image, target = data['image'], data['target']
+            image, target = data["image"], data["target"]
             image = {k: v.to(self.device) for k, v in image.items()}
             target = target.to(self.device)
 
             logits = model(image, output_shape=target.shape[-2:]).squeeze(dim=1)
             mse = F.mse_loss(logits, target)
 
-        metrics = {"MSE" : mse.item(), "RMSE" : torch.sqrt(mse).item()}
+        metrics = {"MSE": mse.item(), "RMSE": torch.sqrt(mse).item()}
         self.log_metrics(metrics)
 
         used_time = time.time() - t
@@ -192,14 +212,18 @@ class RegEvaluator(Evaluator):
         return metrics, used_time
 
     @torch.no_grad()
-    def __call__(self, model, model_name='model'):
+    def __call__(self, model, model_name="model"):
         return self.evaluate(model, model_name)
 
     def log_metrics(self, metrics):
         header = "------- MSE and RMSE --------\n"
-        mse = "-------------------\n" + 'MSE \t{:>7}'.format('%.3f' % metrics['MSE'])+'\n'
-        rmse = "-------------------\n" + 'RMSE \t{:>7}'.format('%.3f' % metrics['RMSE'])
-        self.logger.info(header+mse+rmse)
+        mse = (
+            "-------------------\n"
+            + "MSE \t{:>7}".format("%.3f" % metrics["MSE"])
+            + "\n"
+        )
+        rmse = "-------------------\n" + "RMSE \t{:>7}".format("%.3f" % metrics["RMSE"])
+        self.logger.info(header + mse + rmse)
 
         if self.args.use_wandb and self.args.rank == 0:
             self.wandb.log({"val_MSE": metrics["MSE"], "val_RMSE": metrics["RMSE"]})
