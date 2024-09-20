37e210cbc6bb65c5ac19142e5d1b737a1e50f841
gle-bellier <georges.lebellier@gmail.com>
2024-09-20 18:55:29 +0200

    Refactor losses

--- geofm_bench/utils/losses.py
+++ geofm_bench/utils/losses.py
@@ -1,30 +1,26 @@
 import torch
-from utils.registry import LOSS_REGISTRY
 from torch.nn import functional as F
 
 
-@LOSS_REGISTRY.register()
-def CrossEntropy(cfg):
-    # if cfg["use_weights"]:
-    return torch.nn.CrossEntropyLoss(ignore_index = cfg["ignore_index"]) #, weights = )
+class WeightedCrossEntropy(torch.nn.Module):
+    def __init__(self, ignore_index: int, distribution: list[float]) -> None:
+        self.weights = [1 / w for w in distribution]
 
-@LOSS_REGISTRY.register()
-def WeightedCrossEntropy(cfg):
-    weights = [1/w for w in cfg["distribution"]]
-    loss_weights = torch.Tensor(weights).to("cuda")
-    return torch.nn.CrossEntropyLoss(ignore_index = cfg["ignore_index"], weight = loss_weights)
+        # TODO: remove this to cuda
+        loss_weights = torch.Tensor(self.weights).to("cuda")
+        self.loss = torch.nn.CrossEntropyLoss(
+            ignore_index=ignore_index, weight=loss_weights
+        )
 
-@LOSS_REGISTRY.register()
-def MSELoss(cfg):
-    return torch.nn.MSELoss()
+    def forward(self, logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        return self.loss(logits, target)
 
 
-@LOSS_REGISTRY.register()
 class DICELoss(torch.nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, ignore_index: int) -> None:
         super(DICELoss, self).__init__()
-        self.ignore_index = cfg["ignore_index"]
-    
+        self.ignore_index = ignore_index
+
     def forward(self, logits, target):
         num_classes = logits.shape[1]
 
@@ -33,9 +29,9 @@ class DICELoss(torch.nn.Module):
             probs = torch.sigmoid(logits)
         else:
             probs = F.softmax(logits, dim=1)
-        
-        mask = (target != self.ignore_index)
-        #mask_expand = mask.unsqueeze(1).expand_as(probs)
+
+        mask = target != self.ignore_index
+        # mask_expand = mask.unsqueeze(1).expand_as(probs)
         target = target.clone()
         target[~mask] = 0
 
@@ -44,16 +40,15 @@ class DICELoss(torch.nn.Module):
         else:
             target = F.one_hot(target, num_classes=num_classes)
             target = target.permute(0, 3, 1, 2)
-        
+
         target = target.float() * mask.unsqueeze(1).float()
         intersection = torch.sum(probs * target, dim=(2, 3))
         union = torch.sum(probs + target, dim=(2, 3))
 
-        dice_score = (2. * intersection + 1e-6) / (union + 1e-6)
+        dice_score = (2.0 * intersection + 1e-6) / (union + 1e-6)
         # dice_loss = 1 - dice_score.mean(dim=1).mean()
         valid_dice = dice_score[mask.any(dim=1).any(dim=1)]
         dice_loss = 1 - valid_dice.mean()  # Dice loss is 1 minus the Dice score
-        
+
         return dice_loss
 
-    
