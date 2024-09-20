33b07bee4eb89410b7ef832fdcd558d54581357c
Ritu Yadav <40523539+RituYadav92@users.noreply.github.com>
2024-09-20 15:55:28 +0200

    Update biomassters.py
    switched from skimage io to tifffile imread

--- datasets/biomassters.py
+++ datasets/biomassters.py
@@ -1,1 +1,114 @@
+import numpy as np
+import torch
+import pandas as pd
+import pathlib
+import rasterio
+from tifffile import imread
+from os.path import join as opj
+from .utils import read_tif
+from utils.registry import DATASET_REGISTRY
 
+s1_min = np.array([-25 , -62 , -25, -60], dtype="float32")
+s1_max = np.array([ 29 ,  28,  30,  22 ], dtype="float32")
+s1_mm = s1_max - s1_min
+s2_max = np.array(
+    [19616., 18400., 17536., 17097., 16928., 16768., 16593., 16492., 15401., 15226.,   255.],
+    dtype="float32",
+)
+IMG_SIZE = (256, 256)
+
+def read_imgs(multi_temporal, temp , fname, data_dir):
+    imgs_s1, imgs_s2, mask = [], [], []
+    if multi_temporal:
+        month_list = list(range(12))
+    else:
+        month_list = [temp]
+    
+    for month in month_list:
+        
+        s1_fname = '%s_%s_%02d.tif' % (str.split(fname, '_')[0], 'S1', month)
+        s2_fname = '%s_%s_%02d.tif' % (str.split(fname, '_')[0], 'S2', month)
+
+        s1_filepath = data_dir.joinpath(s1_fname)
+        if s1_filepath.exists():
+            img_s1 = imread(s1_filepath)
+            m = img_s1 == -9999
+            img_s1 = img_s1.astype("float32")
+            img_s1 = (img_s1 - s1_min) / s1_mm
+            img_s1 = np.where(m, 0, img_s1)
+        else:            
+            img_s1 = np.zeros(IMG_SIZE + (4,), dtype="float32")
+        
+        s2_filepath = data_dir.joinpath(s2_fname)
+        if s2_filepath.exists():
+            img_s2 = imread(s2_filepath)
+            img_s2 = img_s2.astype("float32")
+            img_s2 = img_s2 / s2_max
+        else:            
+            img_s2 = np.zeros(IMG_SIZE + (11,), dtype="float32")
+        
+        img_s1 = np.transpose(img_s1, (2, 0, 1))
+        img_s2 = np.transpose(img_s2, (2, 0, 1))
+        imgs_s1.append(img_s1)
+        imgs_s2.append(img_s2)
+        mask.append(False)
+
+    mask = np.array(mask)
+
+    imgs_s1 = np.stack(imgs_s1, axis=1)
+    imgs_s2 = np.stack(imgs_s2, axis=1)
+
+    return imgs_s1, imgs_s2, mask
+
+@DATASET_REGISTRY.register()
+class BioMassters(torch.utils.data.Dataset):
+    def __init__(self, cfg, split):
+        
+        self.root_path = cfg['root_path']
+        self.data_min = cfg['data_min']
+        self.data_max = cfg['data_max']
+        self.multi_temporal = cfg['multi_temporal']
+        self.temp = cfg['temporal']
+        self.split = split
+        
+        self.data_path = pathlib.Path(self.root_path).joinpath(f"{split}_Data_list.csv")
+        self.id_list = pd.read_csv(self.data_path)['chip_id']
+        self.dir_features = pathlib.Path(self.root_path).joinpath("TRAIN/train_features")        
+        self.dir_labels = pathlib.Path(self.root_path).joinpath( "TRAIN/train_agbm")        
+
+    def __len__(self):
+        return len(self.id_list)
+
+    def __getitem__(self, index):
+
+        chip_id = self.id_list.iloc[index]
+        fname = str(chip_id)+'_agbm.tif'
+
+        imgs_s1, imgs_s2, mask = read_imgs(self.multi_temporal, self.temp, fname, self.dir_features)
+        with rasterio.open(self.dir_labels.joinpath(fname)) as lbl:
+            target = lbl.read(1)
+        target = np.nan_to_num(target)
+
+        imgs_s1 = torch.from_numpy(imgs_s1).float()
+        imgs_s2 = torch.from_numpy(imgs_s2).float()
+        target = torch.from_numpy(target).float()
+
+        return {
+            'image': {
+                    'optical': imgs_s2,
+                    'sar' : imgs_s1,
+                    },
+            'target': target,  
+            'metadata': {"masks":mask}
+        }
+
+    @staticmethod
+    def get_splits(dataset_config):
+        dataset_train = BioMassters(cfg=dataset_config, split="train")
+        dataset_val = BioMassters(cfg=dataset_config, split="val")
+        dataset_test = BioMassters(cfg=dataset_config, split="test")
+        return dataset_train, dataset_val, dataset_test
+    
+    @staticmethod
+    def download(dataset_config:dict, silent=False):
+        pass
