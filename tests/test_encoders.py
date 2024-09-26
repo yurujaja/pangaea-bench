5d0ee1e49543917ccac9465b8f54dd3c0680dd71
gle-bellier <georges.lebellier@gmail.com>
2024-09-26 14:36:53 +0200

    Add test for model init

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -1,15 +1,38 @@
+import pytest
 from hydra import compose, initialize
 from hydra.utils import instantiate
-import pytest
 
 from geofm_bench.encoders.base import Encoder
 
+
+# Some encoders configs depend on dataset configs (via interpolation), exclude them for now.
 # TODO: include dofa which depends on dataset.bands
-@pytest.mark.parametrize("config_name", ["croma_joint", "croma_optical", "croma_sar", "scalemae", "gfmswin"])
+# TODO: include prithvi which depends on dataset.multi_temporal
+# TODO: include ssl4eo_mae_sar, ssl4eo_mae_optical, unet_encoder which depends on dataset.img_size
+@pytest.mark.parametrize(
+    "config_name",
+    [
+        "croma_joint",
+        "croma_optical",
+        "croma_sar",
+        "scalemae",
+        "gfmswin",
+        "remoteclip",
+        "satlasnet",
+        "scalemae",
+        "spectralgpt",
+        "ssl4eo_data2vec",
+        "ssl4eo_dino",
+        "ssl4eo_mae_optical",
+        # "ssl4eo_mae_sar",
+        # "ssl4eo_moco",
+        # "unet_encoder",
+    ],
+)
 def test_encoder_init(config_name: str) -> None:
     with initialize(version_base=None, config_path="../configs/encoder/"):
         # config is relative to a module
         encoder_config = compose(config_name=config_name)
         encoder = instantiate(encoder_config)
-        print(encoder)
         assert isinstance(encoder, Encoder)
+
