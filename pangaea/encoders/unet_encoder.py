c9906f46c7b612016f91dbc89b3976327479bc6b
gle-bellier <georges.lebellier@gmail.com>
2024-10-30 22:24:19 +0100

    Add multi temporal unet encoder and corresponding config

--- pangaea/encoders/unet_encoder.py
+++ pangaea/encoders/unet_encoder.py
@@ -60,6 +60,67 @@ class UNet(Encoder):
         pass
 
 
+class UNetMT(Encoder):
+    """
+    Multi Temporal UNet Encoder for Supervised Baseline, to be trained from scratch.
+    It supports single time frame inputs with optical bands
+
+    Args:
+        input_bands (dict[str, list[str]]): Band names, specifically expecting the 'optical' key with a list of bands.
+        input_size (int): Size of the input images (height and width).
+        topology (Sequence[int]): The number of feature channels at each stage of the U-Net encoder.
+
+    """
+
+    def __init__(
+        self,
+        input_bands: dict[str, list[str]],
+        input_size: int,
+        multi_temporal: int,
+        topology: Sequence[int],
+        output_dim: int | list[int],
+        download_url: str,
+        encoder_weights: str | None = None,
+    ):
+        super().__init__(
+            model_name="unet_encoder",
+            encoder_weights=encoder_weights,  # no pre-trained weights, train from scratch
+            input_bands=input_bands,
+            input_size=input_size,
+            embed_dim=0,
+            output_dim=output_dim,
+            output_layers=None,
+            multi_temporal=multi_temporal,
+            multi_temporal_output=False,
+            pyramid_output=True,
+            download_url=download_url,
+        )
+
+        self.in_channels = len(input_bands["optical"])  # number of optical bands
+        self.topology = topology
+
+        self.in_conv = InConv(self.in_channels, self.topology[0], DoubleConv)
+        self.encoder = UNet_Encoder(self.topology)
+
+        self.time_merging = DoubleConv(
+            in_ch=self.in_channels * self.multi_temporal, out_ch=self.in_channels
+        )
+
+    def forward(self, image):
+        x = image["optical"]
+        b, c, t, h, w = x.shape
+        # merge time and channels dimension
+        x = x.reshape(b, c * t, h, w)
+        x = self.time_merging(x)
+
+        feat = self.in_conv(x)
+        output = self.encoder(feat)
+        return output
+
+    def load_encoder_weights(self, logger: Logger) -> None:
+        pass
+
+
 class UNet_Encoder(nn.Module):
     """
     UNet Encoder class that defines the architecture of the encoder part of the UNet.
